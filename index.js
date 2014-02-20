86d0f2cf155f7baa7ab09281ac88eb8871db21de
Ubuntu <ubuntu@ip-172-31-15-141.us-west-1.compute.internal>
2014-02-20 04:50:07 +0000

    Add index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function main(request, response, next) {
 		case 'GET': get(request, response); break;
 		case 'POST': post(request, response); break;
 		case 'DELETE': del(request, response); break;
-		case 'PUT': post(request, response); break;
+		case 'PUT': put(request, response); break;
 	}
 };
 
@@ -36,17 +36,36 @@ function get(request, response) {
 };
 
 function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
 
-	response.end("Logged In\n");
+	var body = request.body;
+	console.log(body);
+	// TODO: read 'name and email from the request.body'
+	 var newSessionId = login.login(body['name'], body['email']);
+	// TODO: set new session id to the 'session_id' cookie in the response
+	 response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+	// replace "Logged In" response with response.end(login.hello(newSessionId));
+	 response.end(login.hello(newSessionId));
+
+	//response.end("Logged In\n");
 };
 
 function del(request, response) {
 	console.log("DELETE:: Logout from the server");
  	// TODO: remove session id via login.logout(xxx)
+	var cookies = request.cookies;
+        console.log(cookies);
+        if ('session_id' in cookies) {
+                var sid = cookies['session_id'];
+                if ( login.isLoggedIn(sid) ) {
+                       // response.setHeader('Set-Cookie', 'session_id=' + sid);
+			login.logout(sid);
+                } else {
+                        response.end("Invalid session_id! Please login again\n");
+                }
+        } else {
+                response.end("Please login via HTTP POST\n");
+        }
+
  	// No need to set session id in the response cookies since you just logged out!
 
   	response.end('Logged out from the server\n');
@@ -56,7 +75,20 @@ function put(request, response) {
 	console.log("PUT:: Re-generate new seesion_id for the same user");
 	// TODO: refresh session id; similar to the post() function
 
-	response.end("Re-freshed session id\n");
+	var cookies = request.cookies;
+	console.log(cookies);
+	if ('session_id' in cookies) {
+		var sid = cookies['session_id'];
+		
+			var newSessionId = login.replaceSessionId(sid);
+			response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+			response.end("Re-freshed session id\n");
+		} else {
+			response.end("Invalid session_id! Please login again\n");
+		} else {
+                response.end("Please login via HTTP POST\n");
+        }
+	
 };
 
 app.listen(8000);
